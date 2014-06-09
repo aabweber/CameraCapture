309d47f7ce18356021c8fab9d340d08e49d90184
Andrey Buldakovskiy <aabweber@gmail.com>
2014-06-09 16:27:47 +0400

    packege name fixes

--- src/android/CameraCapture.java
+++ src/android/CameraCapture.java
@@ -4,6 +4,7 @@ package org.apache.cordova.plugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
