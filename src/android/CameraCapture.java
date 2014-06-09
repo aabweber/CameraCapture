bba847fd18524b2787fae2f5646ab01d9d84c2bf
Andrey Buldakovskiy <aabweber@gmail.com>
2014-06-09 15:51:39 +0400

    packege name fixes

--- src/android/CameraCapture.java
+++ src/android/CameraCapture.java
@@ -1,4 +1,4 @@
-package com.example.;
+package com.aabweber.CameraCapture;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -6,10 +6,36 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.app.Activity;
+import android.content.Intent;
+
 
 public class CameraCapture extends CordovaPlugin {
+	public static final String ACTION_GET = "get";
+
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		try {
+            if (ACTION_GET.equals(action)) {
+                     JSONObject arg_object = args.getJSONObject(0);
+                     Intent calIntent = new Intent(Intent.ACTION_EDIT)
+                .setType("vnd.android.cursor.item/event")
+                .putExtra("beginTime", arg_object.getLong("startTimeMillis"))
+                .putExtra("endTime", arg_object.getLong("endTimeMillis"))
+                .putExtra("title", arg_object.getString("title"))
+                .putExtra("description", arg_object.getString("description"))
+                .putExtra("eventLocation", arg_object.getString("eventLocation"));
 
+               this.cordova.getActivity().startActivity(calIntent);
+               callbackContext.success();
+               return true;
+            }
+            callbackContext.error("Invalid action");
+            return false;
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(e.getMessage());
+            return false;
+        }
 	}
 }
