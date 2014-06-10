e72216b1df281d942b43ae3108795817097d3e4f
Andrey Buldakovskiy <aabweber@gmail.com>
2014-06-10 10:25:29 +0400

    package renamed

--- src/android/CameraCapture.java
+++ src/android/CameraCapture.java
@@ -1,4 +1,4 @@
-package org.apache.cordova.plugin;
+package com.aabweber;
 
 
 import org.apache.cordova.CallbackContext;
@@ -12,6 +12,17 @@ import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
 
+//import static com.googlecode.javacv.cpp.opencv_core.cvFlip;
+//import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
+
+
+//import com.googlecode.javacv.CanvasFrame;
+//import com.googlecode.javacv.FrameGrabber;
+import com.googlecode.javacv.FrameGrabber;
+import com.googlecode.javacv.VideoInputFrameGrabber;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.CanvasFrame;
+
 
 public class CameraCapture extends CordovaPlugin {
     public static final String TAG = "CameraCapture";
@@ -21,6 +32,15 @@ public class CameraCapture extends CordovaPlugin {
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		try {
             if (ACTION_GET.equals(action)) {
+//            	IplImage image;
+                CanvasFrame canvas = new CanvasFrame("Web Cam");
+                FrameGrabber grabber = new VideoInputFrameGrabber(0);
+                grabber.start();
+                IplImage img = grabber.grab();
+//                grabber.grab();
+//                public GrabberShow() {
+//                    canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//                }
             /*
                      JSONObject arg_object = args.getJSONObject(0);
                      Intent calIntent = new Intent(Intent.ACTION_EDIT)
@@ -33,7 +53,12 @@ public class CameraCapture extends CordovaPlugin {
 
                this.cordova.getActivity().startActivity(calIntent);
                */
-               callbackContext.success();
+               
+               JSONObject retVal = new JSONObject();
+//               retVal.put("image", img.toString());
+               retVal.put("test", "123");
+//               callbackContext.error(retVal);
+               callbackContext.success(retVal);
                return true;
             }
             callbackContext.error("Invalid action");
